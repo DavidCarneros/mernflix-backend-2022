fd5cb48bdcb6782588b709d68f4b2531604d0f4d
David Carneros <David.Carneros1@alu.uclm.es>
2022-03-11 16:55:48 +0100

    Added movie model

---
+++ models/Movie.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var MovieSchema = new Schema({
+    country: String,
+    director: String,
+    category: [String],
+    plot: String,
+    poster: String,
+    year: Number,
+    title: String,
+    trailer: String,
+    imdbRating: String,
+    id: Number
+});
+
+module.exports = mongoose.model('Movie', MovieSchema);
