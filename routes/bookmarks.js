ae2ab042a051f7faf3e0c66af5aad7a77c351ec3
David Carneros <David.Carneros1@alu.uclm.es>
2022-03-27 21:23:33 +0200

    Added comments to movie routes

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -9,33 +9,40 @@ var Bookmark = require("../models/Bookmark.js")
 var db = mongoose.connection;
 
 /* GET bookmarks listing from an user by user email.*/
-router.get("/:email", function(req, res, next) {
+router.get("/:email", function (req, res, next) {
     Bookmark.find({ "email": req.params.email }).sort('-addeddate')
-        .populate('movie').exec(function (err, bookmarks){
+        .populate('movie').exec(function (err, bookmarks) {
             if (err) return res.status(500).send(err);
             else res.status(200).json(bookmarks);
         });
 });
 
 /* POST a new bookmark */
-router.post("/", function(req, res, next) {
-    Bookmark.create(req.body, function(err, bookmark) {
-        if (err) return res.status(500).send(err);
-        else res.sendStatus(200);
-    });
+router.post("/", function (req, res, next) {
+    Bookmark.count({ "movie": req.body.movie, "email": req.body.email })
+        .populate("movie").exec(function (err, count) {
+            console.log(count);
+            if (count > 0) return res.status(409).send(err);
+            else {
+                Bookmark.create(req.body, function (err, bookmark) {
+                    if (err) return res.status(500).send(err);
+                    else res.sendStatus(200);
+                });
+            }
+        })
 });
 
 /* PUT an existing bookmark */
-router.put("/:id", function(req, res, next) {
-    Bookmark.findByIdAndUpdate(req.params.id, req.body, function(err, bookmark) {
+router.put("/:id", function (req, res, next) {
+    Bookmark.findByIdAndUpdate(req.params.id, req.body, function (err, bookmark) {
         if (err) return res.status(500).send(err);
         else res.sendStatus(200);
     });
 });
 
 /* DELETE an existing bookmark */
-router.delete("/:id", function(req, res, next) {
-    Bookmark.findByIdAndDelete(req.params.id, function(err, bookmark) {
+router.delete("/:id", function (req, res, next) {
+    Bookmark.findByIdAndDelete(req.params.id, function (err, bookmark) {
         if (err) return res.status(500).send(err);
         else res.sendStatus(200);
     });
--- routes/movies.js
+++ routes/movies.js
@@ -5,22 +5,49 @@ var router = express.Router();
 
 // Models 
 var Movie = require('../models/Movie.js');
+var Comment = require('../models/Movie.js');
+//var Comment = require('../models/Comment.js');
 
 var db = mongoose.connection;
 
 /* GET movies listing. */
-router.get('/', function(req, res) {
-    Movie.find({}, function(err, movies) {
+router.get('/', function (req, res) {
+    Movie.find({}, function (err, movies) {
         if (err) return res.status(500).send(err);
         else res.status(200).json(movies);
     });
 });
 
 /* GET single movie by Id */
-router.get('/:id', function(req, res, next) {
-    Movie.findById(req.params.id, function(err, movie) {
-        if (err) return res.status(500).send(err);
+router.get('/:id', function (req, res, next) {
+    // Find movie by id and join comments collections
+    Movie.findById(req.params.id).populate('comments').exec(function (err, movie) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        } //return res.status(500).send(err);
         else res.status(200).json(movie);
+    });
+});
+
+/* Add comment to a movie by id */
+router.post('/:id/comments', function (req, res, next) {
+    Movie.findById(req.params.id, function (err, movie) {
+        if (err) return res.status(500).send(err);
+        else {
+            // Add comment to movie
+            if (!movie.comments) movie.comments = [];
+            movie.comments.push({
+                text: req.body.text,
+                username: req.body.username,
+                email: req.body.email,
+            });
+            // updates the movie with the new comment
+            movie.save(function (err, movie) {
+                if (err) return res.status(500).send(err);
+                else res.status(200).json(movie);
+            });
+        }
     });
 });
 
